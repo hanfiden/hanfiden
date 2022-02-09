c7e2d90cea9759fad7020f20fdf870668cfe13d3
Han Fiden <fidendu2@gmail.com>
2022-02-09 22:23:34 +0100

    Add new and create method to controller with secured params

--- app/controllers/contact_requests_controller.rb
+++ app/controllers/contact_requests_controller.rb
@@ -1,5 +1,28 @@
 class ContactRequestsController < ApplicationController
   def new
+    @contact_request = ContactRequest.new
+  end
 
+  def create
+    @contact_request = ContactRequest.new(contact_request_params)
+    if @contact_request.save
+
+      flash[:notice] = 'Message envoyé avec succès ! 🎉'
+      redirect_to new_contact_request_path
+    else
+      flash.now[:error] = "Le message n'a pas été envoyé !"
+      render :new
+    end
+  end
+
+  private
+
+  def contact_request_params
+    params.require(:contact_request).permit(:first_name,
+                                            :last_name,
+                                            :email,
+                                            :phone_number,
+                                            :subject,
+                                            :content)
   end
 end
