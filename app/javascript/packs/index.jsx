25f989ea01855b7d503230e938352d2a22863c02
Han Fiden <fidendu2@gmail.com>
2022-02-14 22:44:51 +0100

    Import all javascript files in componenets

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ScrollUp from '../components/ScrollUp'
 import BackgroundHeader from '../components/BackgroundHeader'
+// import DarkMode from '../components/DarkMode'
+import NavbarToggle from '../components/NavbarToggle'
+import AccordionSkills from '../components/AccordionSkills'
+import QualificationTabs from '../components/QualificationTabs'
+import ServicesModal from '../components/ServicesModal'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
